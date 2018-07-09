0b68a384b1fcc5613a8f6d34a55524f3ca7f4c60
MikeTekOn <hugsantos@outlook.com>
2018-07-09 18:20:48 +0100

    Implementation Repository domain

---
+++ Helptec/src/main/java/com/opensoftware/helptec/repository/FuncionarioRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.opensoftware.helptec.repository;
+
+import com.opensoftware.helptec.domain.Funcionario;
+import com.opensoftware.helptec.persistence.PersistenceRepository;
+
+/**
+ *
+ * @author Hugo
+ * @date 9/jul/2018 
+ */
+public class FuncionarioRepository extends PersistenceRepository<Funcionario, Long> {
+
+}
---
+++ Helptec/src/main/java/com/opensoftware/helptec/repository/TipoEquipamentoRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.opensoftware.helptec.repository;
+
+import com.opensoftware.helptec.domain.TipoEquipamento;
+import com.opensoftware.helptec.persistence.PersistenceRepository;
+import com.opensoftware.helptec.valueobjects.Nome;
+
+/**
+ *
+ * @author Hugo
+ * @date 9/jul/2018 
+ */
+public class TipoEquipamentoRepository extends PersistenceRepository<TipoEquipamento, Nome>{
+
+}
