abb7d74c214a7fb99776579644dea338133eb363
MikeTekOn <hugsantos@outlook.com>
2018-07-12 13:49:51 +0100

    add method repository Utilizador for login

--- Helptec/src/main/java/com/opensoftware/helptec/repository/UtilizadorRepository.java
+++ Helptec/src/main/java/com/opensoftware/helptec/repository/UtilizadorRepository.java
@@ -3,17 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.opensoftware.helptec.repository;
 
 import com.opensoftware.helptec.domain.Utilizador;
 import com.opensoftware.helptec.persistence.PersistenceRepository;
+import javax.persistence.Query;
 
 /**
  *
  * @author Hugo
- * @date 9/jul/2018 
+ * @date 9/jul/2018
  */
-public class UtilizadorRepository extends PersistenceRepository<Utilizador, Long>{
+public class UtilizadorRepository extends PersistenceRepository<Utilizador, Long> {
+
+    /**
+     * Search user for login
+     * @param user User to make login
+     * @return true if user exist
+     */
+    public boolean searchUserForLogin(Utilizador user) {
+
+        Query q = this.entityManager().createQuery("SELECT e.funcionario, e.password FROM Utilizador e, Funcionario f WHERE f.name = :func and e.password = :pass");
+        q.setParameter("func", user.funcionario().name());
+        q.setParameter("pass", user.password());
+
+        return q.getResultList().size() == 1;
+    }
 
 }
